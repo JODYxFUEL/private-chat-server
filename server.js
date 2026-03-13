698309b7e31bb5311f9b1e078c81dd20624375d7
JODYxFUEL <138588381+JODYxFUEL@users.noreply.github.com>
2026-03-13 23:17:55 +0530

    Refactor server.js for code style consistency

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+const express = require("express")
+const http = require("http")
+const {Server}=require("socket.io")
 
-const app = express();
-const server = http.createServer(app);
+const app=express()
+const server=http.createServer(app)
 
-const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
-});
+const io=new Server(server,{
+cors:{origin:"*"}
+})
 
-io.on("connection", (socket) => {
+io.on("connection",(socket)=>{
 
-  socket.on("joinRoom", (room) => {
-    socket.join(room);
-  });
+socket.on("joinRoom",(room)=>{
+socket.join(room)
+})
 
-  socket.on("message", ({room, message}) => {
-    io.to(room).emit("message", message);
-  });
+socket.on("message",(data)=>{
+io.to(data.room).emit("message",data)
+})
 
-});
+socket.on("delete",(data)=>{
+io.to(data.room).emit("delete",data.id)
+})
 
-server.listen(3000, () => {
-  console.log("Server running");
-});
+})
+
+server.listen(3000,()=>{
+console.log("Server running")
+})
